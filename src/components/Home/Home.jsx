91d00bfe833613f3025b4638d6b07509f0ef499d
germangrecoventura <85320986+germangrecoventura@users.noreply.github.com>
2022-09-18 21:58:37 -0300

    Add component to display list (#2)

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,11 @@
+import Acoordion from "./Accordion";
+
 const Home = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-sm-3">Lista de Asistencias</div>
+        <Acoordion></Acoordion>
       </div>
     </div>
   );
