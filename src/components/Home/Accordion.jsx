a13284af059d78d974a0de4f33ff02f64ef9e11f
germangrecoventura <germangv1998@gmail.com>
2022-09-18 23:07:22 -0300

    Show attendance list

--- src/components/Home/Accordion.jsx
+++ src/components/Home/Accordion.jsx
@@ -1,7 +1,7 @@
 import API from "../../services/API";
 import { useEffect, useState } from "react";
 
-const Acoordion = (props) => {
+const Acoordion = () => {
   const [assistances, setAssistances] = useState(null);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
@@ -19,40 +19,34 @@ const Acoordion = (props) => {
       });
   }, []);
 
-    {console.log(assistances.result)}
   return (
     <div className="accordion" id="accordionExample">
-      {assistances.result.map((assistant) => {
+      {assistances.result.map((user, i) => {
         return (
           <div className="accordion-item">
-            <h2 className="accordion-header" id="headingOne">
+            <h2 className="accordion-header" id={i}>
               <button
                 className="accordion-button"
                 type="button"
                 data-bs-toggle="collapse"
-                data-bs-target="#collapseOne"
+                data-bs-target={`#collapse${i}`}
                 aria-expanded="true"
-                aria-controls="collapseOne"
+                aria-controls={`collapse${i}`}
               >
-                Accordion Item #1
+                {user.id}
               </button>
             </h2>
             <div
-              id="collapseOne"
+              id={`collapse${i}`}
               className="accordion-collapse collapse show"
-              aria-labelledby="headingOne"
+              aria-labelledby={i}
               data-bs-parent="#accordionExample"
             >
               <div className="accordion-body">
-                <strong>This is the first item's accordion body.</strong> It is
-                shown by default, until the collapse plugin adds the appropriate
-                classNamees that we use to style each element. These classNamees
-                control the overall appearance, as well as the showing and
-                hiding via CSS transitions. You can modify any of this with
-                custom CSS or overriding our default variables. It's also worth
-                noting that just about any HTML can go within the{" "}
-                <code>.accordion-body</code>, though the transition does limit
-                overflow.
+                <h4>Kind: {user.kind}</h4>
+                <h4>Detail: {user.detail}</h4>
+                <h4>CostPerKm: ${user.costPerKm}</h4>
+                <h4>User: {user.assistant.id}</h4>
               </div>
             </div>
           </div>
