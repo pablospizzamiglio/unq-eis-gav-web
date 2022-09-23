8f86259dbccee27e1a038f4277c8c818b54fbf8d
pablospizzamiglio <spizzamiglio.pablo@gmail.com>
2022-09-23 17:05:21 -0300

    Render assistance kind filter and send filtered API requests

--- src/components/Home/Accordion.jsx
+++ src/components/Home/Accordion.jsx
@@ -1,12 +1,27 @@
 import { useEffect, useState } from "react";
 import API from "../../services/API";
-import NotFound from "../NotFound";
+import LoadingError from "../LoadingError";
 import Spinner from "../Spinner";
 
 const Acordion = () => {
   const [assistances, setAssistances] = useState(null);
   const [error, setError] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const kindFilterOptions = [
+    { value: "", text: "--Choose assistance kind--" },
+    { value: "SMALL", text: "Small" },
+    { value: "MEDIUM", text: "Medium" },
+    { value: "LARGE", text: "Large" },
+    { value: "BATTERY", text: "Battery" },
+    { value: "START_UP", text: "Start Up" },
+  ];
+  const [selectedKindFilterOption, setSelectedKindFilterOption] = useState(
+    kindFilterOptions[0].value
+  );
+
+  const handleKindFilterChange = (event) => {
+    setSelectedKindFilterOption(event.target.value);
+  };
 
   useEffect(() => {
     API.getAssistances()
@@ -19,52 +34,84 @@ const Acordion = () => {
       .finally(() => {
         setIsLoading(false);
       });
-  }, []);
+  }, [selectedKindFilterOption]);
+
+  const renderTitle = () => (
+    <div className="row">
+      <h1 className="important-title">Assistances</h1>
+    </div>
+  );
 
   if (isLoading) {
-    return <Spinner fullscreen={false} />;
-  }
-
-  if (error) {
-    return <NotFound />;
+    return (
+      <>
+        {renderTitle()}
+        <div className="container py-4">
+          <Spinner fullscreen={false} />
+        </div>
+      </>
+    );
   }
 
   return (
-    <div className="accordion" id="assistance-accordion">
-      {assistances.result.map((assistance, i) => {
-        return (
-          <div className="accordion-item" key={i}>
-            <h2 className="accordion-header" id={i}>
-              <button
-                className="accordion-button"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target={`#collapse${i}`}
-                aria-expanded="true"
-                aria-controls={`collapse${i}`}
-              >
-                {assistance.detail}
-              </button>
-            </h2>
-            <div
-              id={`collapse${i}`}
-              className="accordion-collapse collapse show"
-              aria-labelledby={i}
-              data-bs-parent="#assistance-accordion"
+    <div className="container">
+      {renderTitle()}
+
+      {!error && (
+        <>
+          <div className="row">
+            <select
+              className="form-select form-select-lg mb-3"
+              aria-label="Assistance kind filter"
+              value={selectedKindFilterOption}
+              onChange={handleKindFilterChange}
             >
-              <div className="accordion-body">
-                <h4>Kind: {assistance.kind}</h4>
-                <h4>Detail: {assistance.detail}</h4>
-                <h4>CostPerKm: ${assistance.costPerKm}</h4>
-                <h4>
-                  User: {assistance.assistant.firstName}{" "}
-                  {assistance.assistant.lastName}
-                </h4>
-              </div>
-            </div>
+              {kindFilterOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.text}
+                </option>
+              ))}
+            </select>
           </div>
-        );
-      })}
+
+          <div className="accordion" id="assistance-accordion">
+            {assistances.result.map((assistance, i) => {
+              return (
+                <div className="accordion-item" key={i}>
+                  <h2 className="accordion-header" id={i}>
+                    <button
+                      className="accordion-button"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#collapse${i}`}
+                      aria-expanded="false"
+                      aria-controls={`collapse${i}`}
+                    >
+                      {assistance.detail}
+                    </button>
+                  </h2>
+                  <div
+                    id={`collapse${i}`}
+                    className="accordion-collapse collapse"
+                    aria-labelledby={i}
+                  >
+                    <div className="accordion-body">
+                      <h4>Kind: {assistance.kind}</h4>
+                      <h4>CostPerKm: ${assistance.costPerKm}</h4>
+                      <h4>
+                        User: {assistance.assistant.firstName}{" "}
+                        {assistance.assistant.lastName}
+                      </h4>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+
+      {error && <LoadingError />}
     </div>
   );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,11 +3,8 @@ import Acordion from "./Accordion";
 const Home = () => {
   return (
     <div className="container">
-      <div className="col-sm-9">
+      <div className="col">
         <div className="row">
-          <h1 className="important-title">Assistances</h1>
-        </div>
-        <div className="row pt-4">
           <Acordion />
         </div>
       </div>
