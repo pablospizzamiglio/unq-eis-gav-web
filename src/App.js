05074e7fd1f64985f5d38ab7d100d0894b0d6a07
germangrecoventura <85320986+germangrecoventura@users.noreply.github.com>
2022-11-05 16:26:31 -0300

    Add navbar (#18)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/js/bootstrap.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar/Navbar";
 import NotFound from "./components/NotFound";
 import OrderUpdater from "./components/OrderUpdater/OrderUpdater";
 
@@ -11,7 +12,7 @@ const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        {/* <NavBar /> */}
+        <Navbar />
         <div className="container p-2">
           <Routes>
             <Route path="/" element={<Home />} />
